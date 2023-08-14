dc14e965ceb3de17463dc989796ffa9557467ba3
Raju George <test@example.com>
2023-08-14 15:11:01 +0530

    trying hybrid again 15

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'hybrid',
     integrations: [tailwind(), react(), sanity({
         projectId: "t9xt5xc2",
         dataset: "production",
         apiVersion: "2023-08-01",
         useCdn: false
     })],
+    adapter: vercel(),
 });
